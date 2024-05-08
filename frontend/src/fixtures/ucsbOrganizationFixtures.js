a14bd58d2b9bd5d1e2f906748eb2b21fcc85ab1b
Brady Gin <bradygin3@gmail.com>
2024-05-08 14:45:11 -0700

    bg - Added UCSBOrganization fixtures

---
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -0,0 +1,37 @@
+const ucsbOrganizationFixtures = {
+    oneOrganization: 
+    [
+        {
+        "orgCode": "SPE",
+        "orgTranslationShort": "SigEp",
+        "orgTranslation": "Sigma Phi Epsilon",
+        "inactive": "false"
+        }
+    ],
+
+    threeOrganizations:
+    [
+        {
+        "orgCode": "SC",
+        "orgTranslationShort": "SigChi",
+        "orgTranslation": "Sigma Chi",
+        "inactive": "false"
+        },
+
+        {
+        "orgCode": "DZ",
+        "orgTranslationShort": "DeltZete",
+        "orgTranslation": "Delta Zeta",
+        "inactive": "false"
+        },
+
+        {
+        "orgCode": "ATO",
+        "orgTranslationShort": "AlphaTau",
+        "orgTranslation": "Alpha Tau Omega",
+        "inactive": "false"
+        },
+    ]
+};
+
+export { ucsbOrganizationFixtures};
