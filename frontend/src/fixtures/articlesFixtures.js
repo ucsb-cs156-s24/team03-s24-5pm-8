21d42fa60edaab67ba5eaa9b7e1488b564333314
Sungchae Park <sungchaepark@169-231-60-228.wireless.ucsb.edu>
2024-05-07 17:33:44 -0700

    sp - added articlesFixtures file

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,46 @@
+const articlesFixtures = {
+    oneArticle:
+    [
+      {
+       "id": 1,
+        "dateAdded": "2022-01-02T12:00:00",
+        "title": "title1",
+        "url": "url1",
+        "explanation": "explanation1",   
+        "email": "email1"   
+      }
+    ],
+
+    threeArticles:
+    [
+        {
+            "id": 2,
+            "dateAdded": "2022-02-02T12:00:00",
+            "title": "title2",
+            "url": "url2",
+            "explanation": "explanation2",   
+            "email": "email2"        
+        },
+
+        {
+            "id": 3,
+            "dateAdded": "2022-03-02T12:00:00",
+            "title": "title3",
+            "url": "url3",
+            "explanation": "explanation3",   
+            "email": "email3"  
+        },
+
+        {
+            "id": 4,
+            "dateAdded": "2022-04-02T12:00:00",  
+            "title": "title4",
+            "url": "url4",
+            "explanation": "explanation4",   
+            "email": "email4"     
+        },
+        
+    ]
+};
+
+export { articlesFixtures };
