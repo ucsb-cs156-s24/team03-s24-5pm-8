de7dff25dc2c4666368388ec2a43b1b647ec99d5
Sungchae Park <sungchaepark@169-231-60-228.wireless.ucsb.edu>
2024-05-10 13:40:49 -0700

    sp- done with component just need renaming

--- frontend/src/main/components/Articles/ArticleForm.js
+++ frontend/src/main/components/Articles/ArticleForm.js
@@ -1,6 +1,7 @@
 import { Button, Form } from 'react-bootstrap';
 import { useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom';
+import { Row, Col } from 'react-bootstrap';
 
 function ArticleForm({ initialContents, submitAction, buttonLabel = "Create" }) {
 
@@ -13,7 +14,11 @@ function ArticleForm({ initialContents, submitAction, buttonLabel = "Create" }) 
     } = useForm(
         { defaultValues: initialContents || {}, }
     );
+    const isodate_regex = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
     // Stryker restore all
+
+    // Stryker disable next-line Regex
+    // const email_regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
    
     const navigate = useNavigate();
 
@@ -21,91 +26,110 @@ function ArticleForm({ initialContents, submitAction, buttonLabel = "Create" }) 
 
     return (
         <Form onSubmit={handleSubmit(submitAction)}>
+        <Row>
+                {initialContents && (
+                    <Col>
+                        <Form.Group className="mb-3" >
+                            <Form.Label htmlFor="id">Id</Form.Label>
+                            <Form.Control
+                                data-testid="ArticleForm-id"
+                                id="id"
+                                type="text"
+                                {...register("id")}
+                                value={initialContents.id}
+                                disabled
+                            />
+                        </Form.Group>
+                    </Col>
+                )}
 
-            {initialContents && (
+        
+            <Col>
                 <Form.Group className="mb-3" >
-                    <Form.Label htmlFor="id">Id</Form.Label>
+                    <Form.Label htmlFor="dateAdded">Date Added(iso format)</Form.Label>
                     <Form.Control
-                        data-testid={testIdPrefix + "-id"}
-                        id="id"
-                        type="text"
-                        {...register("id")}
-                        value={initialContents.id}
-                        disabled
+                        data-testid="ArticleForm-dateAdded"
+                        id="dateAdded"
+                        type="datetime-local"
+                        isInvalid={Boolean(errors.dateAdded)}
+                        {...register("dateAdded", { required: true, pattern: isodate_regex })}
                     />
+                    <Form.Control.Feedback type="invalid">
+                        {errors.dateAdded && 'Date added is required.'}
+                    </Form.Control.Feedback>
                 </Form.Group>
-            )}
+            </Col>
 
-            <Form.Group className="mb-3" >
-                <Form.Label htmlFor="dateAdded">DateAdded (iso format)</Form.Label>
-                <Form.Control
-                    data-testid="DateAdded-localDateTime"
-                    id="dateAdded"
-                    type="datetime-local"
-                    isInvalid={Boolean(errors.dateAdded)}
-                    {...register("dateAdded", { required: true, pattern: isodate_regex })}
-                />
-                <Form.Control.Feedback type="invalid">
-                    {errors.dateAdded && 'DateAdded is required. '}
-                </Form.Control.Feedback>
-            </Form.Group>
-
-            <Form.Group className="mb-3" >
-                <Form.Label htmlFor="title">Name</Form.Label>
-                <Form.Control
-                    data-testid={testIdPrefix + "-title"}
-                    id="title"
-                    type="text"
-                    isInvalid={Boolean(errors.title)}
-                    {...register("title", {
-                        required: "Title is required.",
-                        maxLength : {
+            <Col>
+                <Form.Group className="mb-3" >
+                    <Form.Label htmlFor="title">Title</Form.Label>
+                    <Form.Control
+                        data-testid="ArticleForm-title"
+                        id="title"
+                        type="text"
+                        isInvalid={Boolean(errors.title)}
+                        {...register("title", {
+                            required: "Title is required.",
+                            maxLength : {
                             value: 30,
                             message: "Max length 30 characters"
                         }
-                    })}
-                />
-                <Form.Control.Feedback type="invalid">
-                    {errors.title?.message}
-                </Form.Control.Feedback>
-            </Form.Group>
+                        })}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {errors.title?.message}
+                    </Form.Control.Feedback>
+                </Form.Group>
+            </Col>
+        </Row>
 
-            <Form.Group className="mb-3" >
-                <Form.Label htmlFor="url">Url</Form.Label>
-                <Form.Control
-                    data-testid={testIdPrefix + "-url"}
-                    id="url"
-                    type="text"
-                    isInvalid={Boolean(errors.url)}
-                    {...register("url", {
-                        required: "Url is required."
-                    })}
-                />
-                <Form.Control.Feedback type="invalid">
-                    {errors.url?.message}
-                </Form.Control.Feedback>
-            </Form.Group>
+        <Row>
+            <Col>
+                <Form.Group className="mb-3" >
+                    <Form.Label htmlFor="url">Url</Form.Label>
+                    <Form.Control
+                        data-testid="ArticleForm-url"
+                        id="url"
+                        type="text"
+                        isInvalid={Boolean(errors.url)}
+                        {...register("url", {
+                            required: "Url is required."
+                        })}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {errors.url?.message}
+                    </Form.Control.Feedback>
+                </Form.Group>
+            </Col>
 
-            <Form.Group className="mb-3" >
-                <Form.Label htmlFor="explanation">Explanation</Form.Label>
-                <Form.Control
-                    data-testid={testIdPrefix + "-explanation"}
-                    id="explanation"
-                    type="text"
-                    isInvalid={Boolean(errors.explanation)}
-                    {...register("explanation", {
-                        required: "Explanation is required."
-                    })}
-                />
-                <Form.Control.Feedback type="invalid">
-                    {errors.explanation?.message}
-                </Form.Control.Feedback>
-            </Form.Group>
+            <Col>
+                <Form.Group className="mb-3" >
+                    <Form.Label htmlFor="explanation">Explanation</Form.Label>
+                    <Form.Control
+                        data-testid="ArticleForm-explanation"
+                        id="explanation"
+                        type="text"
+                        isInvalid={Boolean(errors.explanation)}
+                        {...register("explanation", {
+                            required: "Explanation is required.",
+                            maxLength : {
+                            value: 200,
+                            message: "Max length 200 characters"
+                        }
+                        })}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                       {errors.explanation?.message}
+                    </Form.Control.Feedback>
+                </Form.Group>
+            </Col>
+        </Row>
 
+        <Row>
             <Form.Group className="mb-3" >
                 <Form.Label htmlFor="email">Email</Form.Label>
                 <Form.Control
-                    data-testid={testIdPrefix + "-email"}
+                    data-testid="ArticleForm-email"
                     id="email"
                     type="text"
                     isInvalid={Boolean(errors.email)}
@@ -117,6 +141,7 @@ function ArticleForm({ initialContents, submitAction, buttonLabel = "Create" }) 
                     {errors.email?.message}
                 </Form.Control.Feedback>
             </Form.Group>
+        </Row>
 
 
             <Button
@@ -138,4 +163,4 @@ function ArticleForm({ initialContents, submitAction, buttonLabel = "Create" }) 
     )
 }
 
-export default RestaurantForm;
+export default ArticleForm;
--- frontend/src/tests/components/Articles/ArticlesForm.test.js
+++ frontend/src/tests/components/Articles/ArticlesForm.test.js
@@ -1,10 +1,7 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
-import { BrowserRouter as Router } from "react-router-dom";
-
+import { render, waitFor, fireEvent, screen } from "@testing-library/react";
 import ArticleForm from "main/components/Articles/ArticleForm";
 import { articleFixtures } from "fixtures/articleFixtures";
-
-import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const mockedNavigate = jest.fn();
 
@@ -13,90 +10,127 @@ jest.mock('react-router-dom', () => ({
     useNavigate: () => mockedNavigate
 }));
 
+
 describe("ArticleForm tests", () => {
-    const queryClient = new QueryClient();
 
-    const expectedHeaders = ["Name", "Description"];
-    const testId = "RestaurantForm";
+    test("renders correctly", async () => {
 
-    test("renders correctly with no initialContents", async () => {
         render(
-            <QueryClientProvider client={queryClient}>
-                <Router>
-                    <RestaurantForm />
-                </Router>
-            </QueryClientProvider>
+            <Router  >
+                <ArticleForm />
+            </Router>
         );
+        // await screen.findByText(/Id/);
+        await screen.findByText(/Create/);
+    });
 
-        expect(await screen.findByText(/Create/)).toBeInTheDocument();
 
-        expectedHeaders.forEach((headerText) => {
-            const header = screen.getByText(headerText);
-            expect(header).toBeInTheDocument();
-        });
+    test("renders correctly when passing in a Article", async () => {
+
+        render(
+            <Router  >
+                <ArticleForm initialContents={articleFixtures.oneArticle[0]} />
+            </Router>
+        );
+        await screen.findByTestId(/ArticleForm-id/);
+        expect(screen.getByText(/Id/)).toBeInTheDocument();
+        expect(screen.getByTestId(/ArticleForm-id/)).toHaveValue("1");
+    });
+
+
+    test("Correct Error messsages on bad input", async () => {
+
+        render(
+            <Router  >
+                <ArticleForm />
+            </Router>
+        );
+        await screen.findByTestId("ArticleForm-title");
+        const titleField = screen.getByTestId("ArticleForm-title");
+        const explanationField = screen.getByTestId("ArticleForm-explanation");
+        const dateAddedField = screen.getByTestId("ArticleForm-dateAdded");
+        const submitButton = screen.getByTestId("ArticleForm-submit");
+
+        fireEvent.change(titleField, { target: { value: 'morethanthirtycharactersforthetitlecausesanerrorjustlikethis' } });
+        fireEvent.change(explanationField, { target: { value: '00afjeadfkijaoweijfoiwaejfaiojfaoifjdkfjadkfjfkdjajdslkfjakdfjifjknfvfghfjkdfjhgjfkdfjhgjfkdfjghjfkfjghjfkjghjfkjghjfkjghjfkjghjfkjghjfkjgjfkjgfkjghjfkfjkfjgfkfjgjfkjgjfkfjkfjgjfkfjghjfkdfjjkfjjkfjjkawjfakwefgjdhbcvhjfgegfjhdjshdjshdjshdjshdjshdjsdhjshdjshdjshdjshdjshdjshdjshdjshdjshdjshdjshdjshdjhsjdjshdjshdjshdjshdjssjdhsjdhjsdhdjshdjshdjshjsdhjshdjshdjshdjhdjshdjshdjshfdjfhakwefhsjdfhajkfhkajdfhalkjdfhkdjfhkjsdhfkjsdhfsjfhkjdshfksjdhfksjdhfksjdfhksjdhfskjdfkhfskdjfhksjdhfksjdhfksjdfhskjfhskjdhfskjdfhdksjfhskjdfhdksjhfskjdhfdskjhfjdsf' } });
+        fireEvent.change(dateAdded, { target: { value: "bad-input" } });
+        fireEvent.click(submitButton);
+
+        await screen.findByText(/Max length 30 characters/);
+        await screen.findByText(/Max length 200 characters/);
+    });
+
+    test("Correct Error messsages on missing input", async () => {
+
+        render(
+            <Router  >
+                <ArticleForm />
+            </Router>
+        );
+        await screen.findByTestId("ArticleForm-submit");
+        const submitButton = screen.getByTestId("ArticleForm-submit");
+
+        fireEvent.click(submitButton);
+
+        await screen.findByText(/Date added is required./);
+        await screen.findByText(/Title is required./);
+        await screen.findByText(/Url is required./);
+        await screen.findByText(/Explanation is required./);
+        await screen.findByText(/Email is required./);
 
     });
 
-    test("renders correctly when passing in initialContents", async () => {
+    test("No Error messsages on good input", async () => {
+        const mockSubmitAction = jest.fn();
+
         render(
-            <QueryClientProvider client={queryClient}>
-                <Router>
-                    <RestaurantForm initialContents={restaurantFixtures.oneRestaurant} />
-                </Router>
-            </QueryClientProvider>
+            <Router  >
+                <ArticleForm submitAction={mockSubmitAction} />
+            </Router>
         );
+        await screen.findByTestId("ArticleForm-email");
+        const emailField = screen.getByTestId("ArticleForm-email");
+        const titleField = screen.getByTestId("ArticleForm-title");
+        const urlField = screen.getByTestId("ArticleForm-url");
+        const explanationField = screen.getByTestId("ArticleForm-explanation");
+        const dateAddedField = screen.getByTestId("ArticleForm-dateAdded");
 
-        expect(await screen.findByText(/Create/)).toBeInTheDocument();
+        const submitButton = screen.getByTestId("ArticleForm-submit");
 
-        expectedHeaders.forEach((headerText) => {
-            const header = screen.getByText(headerText);
-            expect(header).toBeInTheDocument();
-        });
+        fireEvent.change(dateAddedField, { target: { value: "2022-01-02T12:00" } });
+        fireEvent.change(titleField, { target: { value: "Computer Science" } });
+        fireEvent.change(urlField, { target: { value: "google.com" } });
+        fireEvent.change(explanationField, { target: { value: "There are so many homeoworks." } });
+        fireEvent.change(emailField, { target: { value: "spark686@ucsb.edu" } });
+        fireEvent.click(submitButton);
 
-        expect(await screen.findByTestId(`${testId}-id`)).toBeInTheDocument();
-        expect(screen.getByText(`Id`)).toBeInTheDocument();
+        await waitFor(() => expect(mockSubmitAction).toHaveBeenCalled());
+
+        expect(screen.queryByText(/Date added is required./)).not.toBeInTheDocument();
+        expect(screen.queryByText(/Title is required./)).not.toBeInTheDocument();
+        expect(screen.queryByText(/Url is required./)).not.toBeInTheDocument();
+        expect(screen.queryByText(/Explanation is required./)).not.toBeInTheDocument();
+        expect(screen.queryByText(/Email is required./)).not.toBeInTheDocument();
+
+        expect(screen.queryByText(/Max length 30 characters./)).not.toBeInTheDocument();
+        expect(screen.queryByText(/Date Added must be in the format YYYY-MM-DDTHH:MM:SS/)).not.toBeInTheDocument();
     });
 
 
     test("that navigate(-1) is called when Cancel is clicked", async () => {
+
         render(
-            <QueryClientProvider client={queryClient}>
-                <Router>
-                    <RestaurantForm />
-                </Router>
-            </QueryClientProvider>
+            <Router  >
+                <ArticleForm />
+            </Router>
         );
-        expect(await screen.findByTestId(`${testId}-cancel`)).toBeInTheDocument();
-        const cancelButton = screen.getByTestId(`${testId}-cancel`);
+        await screen.findByTestId("ArticleForm-cancel");
+        const cancelButton = screen.getByTestId("ArticleForm-cancel");
 
         fireEvent.click(cancelButton);
 
         await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith(-1));
-    });
 
-    test("that the correct validations are performed", async () => {
-        render(
-            <QueryClientProvider client={queryClient}>
-                <Router>
-                    <RestaurantForm />
-                </Router>
-            </QueryClientProvider>
-        );
-
-        expect(await screen.findByText(/Create/)).toBeInTheDocument();
-        const submitButton = screen.getByText(/Create/);
-        fireEvent.click(submitButton);
-
-        await screen.findByText(/Name is required/);
-        expect(screen.getByText(/Description is required/)).toBeInTheDocument();
-
-        const nameInput = screen.getByTestId(`${testId}-name`);
-        fireEvent.change(nameInput, { target: { value: "a".repeat(31) } });
-        fireEvent.click(submitButton);
-
-        await waitFor(() => {
-            expect(screen.getByText(/Max length 30 characters/)).toBeInTheDocument();
-        });
     });
 
 });
