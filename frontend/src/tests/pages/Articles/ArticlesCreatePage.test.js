b04e528fc84e0a12fe170b0741fdecb7409b9499
Sungchae Park <sungchaepark@169-231-60-228.wireless.ucsb.edu>
2024-05-11 16:02:37 -0700

    sp - testing create page

--- frontend/src/tests/pages/Articles/ArticlesCreatePage.test.js
+++ frontend/src/tests/pages/Articles/ArticlesCreatePage.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, waitFor, fireEvent, screen } from "@testing-library/react";
 import ArticlesCreatePage from "main/pages/Articles/ArticlesCreatePage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
@@ -8,24 +8,62 @@ import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
 
+const mockToast = jest.fn();
+jest.mock('react-toastify', () => {
+    const originalModule = jest.requireActual('react-toastify');
+    return {
+        __esModule: true,
+        ...originalModule,
+        toast: (x) => mockToast(x)
+    };
+});
+
+const mockNavigate = jest.fn();
+jest.mock('react-router-dom', () => {
+    const originalModule = jest.requireActual('react-router-dom');
+    return {
+        __esModule: true,
+        ...originalModule,
+        Navigate: (x) => { mockNavigate(x); return null; }
+    };
+});
+
 describe("ArticlesCreatePage tests", () => {
 
-    const axiosMock = new AxiosMockAdapter(axios);
+    const axiosMock =new AxiosMockAdapter(axios);
 
-    const setupUserOnly = () => {
+    beforeEach(() => {
         axiosMock.reset();
         axiosMock.resetHistory();
         axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
         axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
-    };
+    });
 
-    const queryClient = new QueryClient();
-    test("Renders expected content", () => {
-        // arrange
+    test("renders without crashing", () => {
+        const queryClient = new QueryClient();
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <ArticlesCreatePage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+    });
 
-        setupUserOnly();
-       
-        // act
+    test("when you fill in the form and hit submit, it makes a request to the backend", async () => {
+
+        const queryClient = new QueryClient();
+        const article = {
+            id: 17,
+            dateAdded: "2022-02-02T00:00",
+            title: "Groundhog Day",
+            url: "google.com",
+            explanation: "search groundhog day from google", 
+            email: "example@gmail.com"
+        };
+
+        axiosMock.onPost("/api/articles/post").reply( 202, article );
+
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
@@ -34,8 +72,43 @@ describe("ArticlesCreatePage tests", () => {
             </QueryClientProvider>
         );
 
-        // assert
-        expect(screen.getByText("Create page not yet implemented")).toBeInTheDocument();
+        await waitFor(() => {
+            expect(screen.getByTestId("ArticlesForm-dateAdded")).toBeInTheDocument();
+        });
+
+        const dateAddedField = screen.getByTestId("ArticlesForm-dateAdded");
+        const titleField = screen.getByTestId("ArticlesForm-title");
+        const urlField = screen.getByTestId("ArticlesForm-url");
+        const explanationField = screen.getByTestId("ArticlesForm-explanation");
+        const emailField = screen.getByTestId("ArticlesForm-email");
+
+        const submitButton = screen.getByTestId("ArticlesForm-submit");
+
+        fireEvent.change(dateAddedField, { target: { value: '2022-02-02T00:00' } });
+        fireEvent.change(titleField, { target: { value: 'Groundhog Day' } });
+        fireEvent.change(urlField, { target: { value: 'google.com' } });
+        fireEvent.change(explanationField, { target: { value: 'search groundhog day from google' } });
+        fireEvent.change(emailField, { target: { value: 'example@gmail.com' } });
+
+
+        expect(submitButton).toBeInTheDocument();
+
+        fireEvent.click(submitButton);
+
+        await waitFor(() => expect(axiosMock.history.post.length).toBe(1));
+
+        expect(axiosMock.history.post[0].params).toEqual(
+            {
+            "dateAdded": "2022-02-02T00:00",
+            "title": "Groundhog Day",
+            "url": "google.com",
+            "explanation": "search groundhog day from google",
+            "email": "example@gmail.com"
+        });
+
+        expect(mockToast).toBeCalledWith("New article Created - id: 17 title: Groundhog Day");
+        expect(mockNavigate).toBeCalledWith({ "to": "/articles" });
     });
+
 
 });
