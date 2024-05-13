9101f6cb2a87fcf5ffd44a5b3a3124da3f3ffc3a
Karsten Lansing <karstenlansing@ucsb.edu>
2024-05-13 14:54:45 -0700

    lets hope

--- frontend/src/tests/pages/RecommendationRequest/RecommendationRequestCreatePage.test.js
+++ frontend/src/tests/pages/RecommendationRequest/RecommendationRequestCreatePage.test.js
@@ -7,7 +7,6 @@ import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
-import { reduceHooks } from "react-table";
 
 const mockToast = jest.fn();
 jest.mock('react-toastify', () => {
